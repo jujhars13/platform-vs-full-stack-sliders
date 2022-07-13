c3017307d4aad104dd3e692a8651c5880f328e5f
Jujhar Singh <jujhar.singh@thoughtworks.com>
2022-07-13 13:30:25 +0100

    sets up project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # platform-vs-full-stack-sliders
 Visualising when you should rely on a platform team vs build it you run it
+
+## Local Dev
+
+```bash
+# OPTIONAL use the awesome `reload` which auto-refreshes your browser on change using websockets
+# `npm install -g webpack webpack-cli reload`
+
+# install dev and app deps
+npm install
+
+# in the root of the repo
+node_modules/.bin/webpack --watch
+(cd docs && reload -e "html|js|css|json|yml")
+
+# browse to http://localhost:8080/
+```
+
+## Licence
+
+Fill your boots
+
+[GNU GPL](LICENSE)
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: process.env?.NODE_ENV ? process.env?.NODE_ENV : "development",
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "docs"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/css", to: path.resolve(__dirname, "docs/css") },
+        { from: "src/img", to: path.resolve(__dirname, "docs/img") },
+        {
+          from: "src/favicon.ico",
+          to: path.resolve(__dirname, "docs/favicon.ico"),
+        },
+        {
+          from: "src/robots.txt",
+          to: path.resolve(__dirname, "docs/robots.txt"),
+        },
+      ],
+    }),
+  ],
+};
